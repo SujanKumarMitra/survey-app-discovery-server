f8eafafc2b76e5d04dca3ffc5d8a5e6f9ff41146
Sujan Kumar Mitra <mitrakumarsujan@gmail.com>
2020-12-05 08:25:08 +0530

    changes: added discovery server configuration added dev profile properties converted .properties to .yml

---
+++ src/main/java/com/github/mitrakumarsujan/discoveryserver/configuration/DiscoveryServerConfiguration.java
@@ -0,0 +1,9 @@
+package com.github.mitrakumarsujan.discoveryserver.configuration;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEurekaServer
+public class DiscoveryServerConfiguration {
+}
